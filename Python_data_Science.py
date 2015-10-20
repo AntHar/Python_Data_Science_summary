71b0b9a63f34702d2351e7f5f81160ab39f5679b
Ignacio Carracedo <nachocarracedo@users.noreply.github.com>
2015-10-20 11:29:24 -0400

    Update Python_data_Science.py

--- Python_data_Science.py
+++ Python_data_Science.py
@@ -1,5 +1,6 @@
-#numpy, getting started
+#numpy, getting started. Matrix and vectors
 import numpy
 #open csv, set all values as strings (U75) and skip header
-nfl = numpy.genfromtxt(f, delimiter=",", dtype="U75", skip_header=1)
-row_four=nfl[3,:]
+nfl = numpy.genfromtxt(f, delimiter=",", dtype="U75", skip_header=1) #array object numpy
+row_four=nfl[3,:] #array object numpy
+some_columns_row_four=row_four[2:4] #array object numpy
