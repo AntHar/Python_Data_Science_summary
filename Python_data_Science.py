0a435722c9cd530d47c2f16f3f3935d97ac82264
Ignacio Carracedo <nachocarracedo@users.noreply.github.com>
2015-10-20 11:30:24 -0400

    Update Python_data_Science.py

--- Python_data_Science.py
+++ Python_data_Science.py
@@ -4,3 +4,6 @@ import numpy
 nfl = numpy.genfromtxt(f, delimiter=",", dtype="U75", skip_header=1) #array object numpy
 row_four=nfl[3,:] #array object numpy
 some_columns_row_four=row_four[2:4] #array object numpy
+#size of matrix or array
+print (nfl.shape)
+print (row_four.shape)
