c14e1d78397db59a48bb0926987e7ba0ae41205a
Ignacio Carracedo <nachocarracedo@users.noreply.github.com>
2015-10-27 13:06:32 -0400

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -2,3 +2,10 @@ import matplotlib.pyplot as plt
 #create plot
 plt.scatter(month, temperature)
 plt.show()
+# line chart. Remember to sort x values first!
+forest_fires = forest_fires.sort(["temp"])
+plt.plot(forest_fires["temp"], forest_fires["area"])
+plt.xlabel("Wind speed when fire started")
+plt.ylabel("Area consumed by fire")
+plt.title("Wind speed vs fire area")
+plt.show()
