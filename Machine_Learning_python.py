bdc3d02aa255d94ae10424c2145521d7a4f7499d
Ignacio Carracedo <nachocarracedo@users.noreply.github.com>
2015-11-16 20:50:24 -0500

    Update Machine_Learning_python.py

--- Machine_Learning_python.py
+++ Machine_Learning_python.py
@@ -1,5 +1,17 @@
+#mean, sd
+mean = nba_stats["pf"].mean()
+std_dev = nba_stats["pf"].std()
 #skew, kurtosis and modality (characteristics of histograms)
 from scipy.stats import skew
 from scipy.stats import kurtosis
 positive_skew=skew(array)
+#normal distribution
+from scipy.stats import norm
+points = np.arange(-1, 1, 0.01) #vector for -1 to 1 in increments of 0.01
+probabilities = norm.pdf(points, 0, .3) #normal distribution (with probability) of points with mean=0 and sd=0.3
+plt.plot(points, probabilities)
+#correlation
+from scipy.stats.stats import pearsonr
+r, p_value = pearsonr(nba_stats["fga"], nba_stats["pts"])
+
 
