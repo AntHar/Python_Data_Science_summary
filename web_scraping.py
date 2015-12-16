a991a40c2e2b51f4cba88abf77f8a3f93f828dba
Ignacio Carracedo <nachocarracedo@users.noreply.github.com>
2015-12-16 13:54:16 -0500

    Update web_scraping.py

--- web_scraping.py
+++ web_scraping.py
@@ -1,5 +1,10 @@
 import requests #downloads webpage
 import bs4 #extract info
 
+# download page
 res = requests.get('https://www.google.com/search?q=' + ''.join(sys.argv[1:]) + '&tbm=nws')
-################ res.raise_for_status()
+# check if there was an error
+try:
+  res.raise_for_status()
+except Exception as exc:
+  print(' There was a problem: %s' % (exc))
