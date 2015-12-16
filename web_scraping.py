93688701a007a7b7df1a38aa4121ce60bec869a1
Ignacio Carracedo <nachocarracedo@users.noreply.github.com>
2015-12-16 13:48:45 -0500

    Update web_scraping.py

--- web_scraping.py
+++ web_scraping.py
@@ -1,1 +1,5 @@
-#
+import requests #downloads webpage
+import bs4 #extract info
+
+res = requests.get('https://www.google.com/search?q=' + ''.join(sys.argv[1:]) + '&tbm=nws')
+################ res.raise_for_status()
